99aaad4200dbd85a5f78e3dbc98a921305662ebd
Isaac Shoebottom <ir.shoebottom@gmail.com>
2024-05-23 09:34:51 -0300

    Made simpler, as padding tends to always be 0xFF and new size can be computed

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
 use std::fs::File;
 use std::io::Write;
-use clap::Parser;
-use unbytify::unbytify;
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    filename: String,
-    #[arg(short, long)]
-    size: String,
-    #[arg(short, long)]
-    padding: String,
-}
 
 fn parse_filename(filename: &str) -> File {
     let result = File::options().read(true).write(true).append(true).open(&filename);
@@ -25,37 +12,36 @@ fn parse_filename(filename: &str) -> File {
     }
 }
 
-fn parse_size(size: &str) -> usize {
-    let result = unbytify(size);
-    match result {
-        Ok(size) => size as usize,
-        Err(_) => {
-            eprintln!("Invalid size: {}", size);
+fn parse_args(file_str: &Option<String>) -> &String {
+    match file_str {
+        Some(file_str) => file_str,
+        None => {
+            eprintln!("No file provided");
             std::process::exit(1);
         }
     }
 }
 
-fn parse_padding(padding: &str) -> u8 {
-    let result = padding.parse::<u8>();
-    match result {
-        Ok(padding) => padding,
-        Err(_) => {
-            eprintln!("Invalid padding: {}", padding);
-            std::process::exit(1);
-        }
+fn next_power_of_2(n: usize) -> usize {
+    let mut size = 2usize;
+    while size < n {
+        size *= 2;
     }
+    size
 }
 
 fn main() {
-    let args = Args::parse();
-    let mut file = parse_filename(&args.filename);
-    let new_size = parse_size(&args.size);
-    let padding = parse_padding(&args.padding);
+    let padding = 0xFF;
+
+    let filename = std::env::args().nth(1);
+    let mut file = parse_filename(parse_args(&filename));
     let current_size = file.metadata().unwrap().len() as usize;
 
-    println!("File: {}", args.filename);
+    println!("File: {}", filename.unwrap());
     println!("Current size: {}", current_size);
+
+    // Find next power of 2
+    let new_size = next_power_of_2(current_size);
     println!("New size: {}", new_size);
 
     // if new size is bigger than current size, fill with padding until new size
